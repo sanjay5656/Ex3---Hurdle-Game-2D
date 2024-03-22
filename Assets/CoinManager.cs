224a54270743691aca2783c3f87874af301b0e17
sanjay5656 <sanjaysanjay23ai@gmail.com>
2024-03-22 21:00:48 +0530

    s

--- Assets/CoinManager.cs
+++ Assets/CoinManager.cs
@@ -1,1 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+using UnityEngine.UI;
+
+public class CoinManager : MonoBehaviour
+{
+    public int coincount;
+    public Text value;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        value.text = coincount.ToString();
+    }
+}
 
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,1 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class PlayerScript : MonoBehaviour
+{
+   public float speed;
+   public float jumpforce;
+   private Rigidbody2D rb;
+   public CoinManager cc;
+
+   void Start()
+   {
+       rb = GetComponent<Rigidbody2D>();
+   }
+
+   void Update()
+   {
+       float moveinp = Input.GetAxisRaw("Horizontal");
+       transform.position += new Vector3(moveinp, 0, 0) * speed * Time.deltaTime;
+
+       if (Input.GetKeyDown(KeyCode.Space)&&Mathf.Abs(rb.velocity.y) < 0.001f)
+       {
+           rb.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+       }
+   }
+
+   private void OnTriggerEnter2D(Collider2D collision)
+   {
+       if (collision.CompareTag("Destroy"))
+       {
+           cc.coincount++;
+           Destroy(collision.gameObject);
+       }
+   }
+}
 
